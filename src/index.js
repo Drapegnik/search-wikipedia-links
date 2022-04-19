6a7399e54777b9bb0a678c1f38d710e9a6bcc464
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2022-04-20 00:08:45 +0200

    feat: add initial implementation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
 import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
+import { useState, useCallback, useMemo, Fragment } from 'react';
 
 /* 
  As an user, I should be able:
  - to search Wikipedia links with a search term
  - to keep a distinct history list of my last five search terms + the time when the last search happened
 **/
+const WIKI_HOST = 'https://en.wikipedia.org/wiki/';
+
+const fetchData = (searchTerm) => {
+const urlSearch =
+    "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + searchTerm;
+
+return fetch(urlSearch, { mode: "cors" })
+    .then((response) => response.json())
+};
+  
+
 const WikepediaLinksView = () => {
+  const [history, setHistory] = useState({});
   const [searchTerm, setSearchTerms] = useState("");
   const [fetchedAt, setFetchedAt] = useState(new Date());
   const [links, setLinks] = useState([]);
-
-  const fetchData = () => {
-    const urlSearch =
-      "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" +
-      searchTerm;
-    fetch(urlSearch, { mode: "cors" })
-      .then((response) => response.json())
-      .then((response) => {
-        setFetchedAt(new Date());
-        setLinks(response[1]);
-      });
-  };
+  
+  const handleSubmit = useCallback(async (query) => {
+    const response = await fetchData(query);
+    
+    const date = new Date();
+    setFetchedAt(date);
+    setLinks(response[1]);
+    
+    const nextHistory = {
+      ...history,
+      [query]: date.getTime(),
+    }
+    
+    setHistory(nextHistory);
+  }, [setFetchedAt, setLinks, history, setHistory]);
+  
+  const handleHistoryClick = useCallback((historySearch) => {
+    setSearchTerms(historySearch);
+    handleSubmit(historySearch);
+  }, [setSearchTerms, handleSubmit]);
+  
+  const historyList = useMemo(() => {
+    const kv = Object.entries(history);
+    kv.sort((a, b) => b[1] - a[1]);
+    return kv.slice(0, 5);
+  }, [history]);
 
   return (
     <div className="container">
@@ -34,16 +60,21 @@ const WikepediaLinksView = () => {
             setSearchTerms(e.target.value);
           }}
         />
-        <button onClick={() => fetchData()}>Search</button>
+        <button onClick={() => handleSubmit(searchTerm)}>Search</button>
+      </div>
+      <div>
+        <ul>
+        {historyList.map(([query, date]) => <li key={query} onClick={() => handleHistoryClick(query)}>{query} - {date}</li>)}
+        </ul>
       </div>
       <div>
         <dl>
           <dt>Your Wikipedia links (fetched at {fetchedAt.toISOString()}):</dt>
           <dd>
             {links.map((l) => (
-              <>
-                {l} <br />
-              </>
+              <Fragment key={l}>
+                <a href={`${WIKI_HOST}${l}`} target="_blank">{l}</a> <br />
+              </Fragment>
             ))}
           </dd>
           <dt>Previous search term:</dt>
